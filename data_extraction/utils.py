fbbd39bb80d798f9da02b3bee6c9ead05a7fbde3
Igor Vaiman <gosha.vaiman@gmail.com>
2022-07-22 13:45:04 +0400

    hotfix 2

--- data_extraction/utils.py
+++ data_extraction/utils.py
@@ -9,7 +9,7 @@ def parse_rusdraw_datetime(rusdraw: Bank) -> datetime:
     usec = rusdraw['usec']  # this sometimes may exceed 10^6, so we parse it separately
     string = f"{yymmdd:06} {hhmmss:06}"
     try:
-        coarse_dt = datetime.strptime(string, r"%y%m%d %H%M%S %f")
+        coarse_dt = datetime.strptime(string, r"%y%m%d %H%M%S")
         return coarse_dt + timedelta(microseconds=usec)
     except Exception:
         raise RuntimeError(f"Failed to parse rusdraw datetime: {string = } {yymmdd = } {hhmmss = } {usec = }")
